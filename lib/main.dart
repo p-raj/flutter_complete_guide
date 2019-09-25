84fcf64fccfd8d29ef6a4c13786723ef6d5d8ab1
Peeyush <praj@teramatrix.in>
2019-09-25 16:23:15 +0530

    lect 02: working with columns and children widgets

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,25 @@ void main() => runApp(App());
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    List<String> q;
+    q = ['Hello', 'World'];
+
     return MaterialApp(
         // core widget that would be
         // initiated as soon as the application
         // initialized
         home: Scaffold(
       backgroundColor: Colors.white,
-      body: Text('World'),
+      body: Column(
+        children: <Widget>[
+          Text(q[0]),
+          Text(q[1]),
+          RaisedButton(
+            child: Text('Submit'),
+            onPressed: null,
+          )
+        ],
+      ),
       appBar: AppBar(
         backgroundColor: Colors.blue,
         title: Text('Hello'),
