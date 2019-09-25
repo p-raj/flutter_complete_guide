8cbb97d43fa71401dacea9e95a2ff94bc60b51cd
Peeyush <praj@teramatrix.in>
2019-09-25 16:47:52 +0530

    lect 04: stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(App());
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
 
+class AppState extends State<App> {
   // functions for the class
   void answerQuestions() {
-    print('Answer chosen');
+    setState(() {
+      qindex += 1;
+    });
   }
+
+  // indexer
+  int qindex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -22,11 +33,10 @@ class App extends StatelessWidget {
       backgroundColor: Colors.white,
       body: Column(
         children: <Widget>[
-          Text(q[0]),
-          Text(q[1]),
+          Text(q.elementAt(qindex)),
           RaisedButton(
             child: Text('Submit'),
-            onPressed: () =>this.answerQuestions(),
+            onPressed: () => this.answerQuestions(),
           )
         ],
       ),
